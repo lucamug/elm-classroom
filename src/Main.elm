69a92e59ddd341d7c366c3c973ce57cbb0c8518c
Luca <lucamug@gmail.com>
2022-10-21 16:24:14 +0900

    no message

--- src/Main.elm
+++ src/Main.elm
@@ -43,6 +43,8 @@ type alias PermanentState =
     { x : String
     , y : String
     , attendees : Dict.Dict String String
+    , invitations : Dict.Dict String String
+    , frames : Dict.Dict String String
     }
 
 
@@ -54,6 +56,8 @@ init flags =
             { x = "4"
             , y = "3"
             , attendees = Dict.fromList [ ( "1", "Mark" ), ( "2", "John" ) ]
+            , invitations = Dict.fromList [ ( "1", "Mark" ), ( "2", "John" ) ]
+            , frames = Dict.fromList [ ( "1", "Mark" ), ( "2", "John" ) ]
             }
 
         permanentState : PermanentState
@@ -85,15 +89,19 @@ type Modality
 codecPermanentState : Codec.Codec PermanentState
 codecPermanentState =
     Codec.object
-        (\x y attendees ->
+        (\x y attendees invitations frames ->
             { x = x
             , y = y
             , attendees = attendees
+            , invitations = invitations
+            , frames = frames
             }
         )
         |> Codec.field "x" .x Codec.string
         |> Codec.field "y" .y Codec.string
         |> Codec.field "a" .attendees (Codec.dict Codec.string)
+        |> Codec.field "i" .invitations (Codec.dict Codec.string)
+        |> Codec.field "f" .frames (Codec.dict Codec.string)
         |> Codec.buildObject
 
 
@@ -111,15 +119,16 @@ update msg model =
         MsgChangeY string ->
             ( { model | permanentState = { permanentState | y = string } }, Cmd.none )
 
-        MsgChangeAttendee id name ->
-            ( { model
-                | permanentState =
-                    { permanentState
-                        | attendees = Dict.insert id name permanentState.attendees
-                    }
-              }
-            , Cmd.none
-            )
+        MsgChangeValue value id name ->
+            case value of
+                ValueAttendee ->
+                    ( { model | permanentState = { permanentState | attendees = Dict.insert id name permanentState.attendees } }, Cmd.none )
+
+                ValueInvitation ->
+                    ( { model | permanentState = { permanentState | invitations = Dict.insert id name permanentState.invitations } }, Cmd.none )
+
+                ValueFrame ->
+                    ( { model | permanentState = { permanentState | frames = Dict.insert id name permanentState.frames } }, Cmd.none )
 
         MsgChangeModality modality ->
             ( { model | modality = modality }, Cmd.none )
@@ -128,8 +137,14 @@ update msg model =
 type Msg
     = MsgChangeX String
     | MsgChangeY String
-    | MsgChangeAttendee String String
+    | MsgChangeValue Value String String
     | MsgChangeModality Modality
+
+
+type Value
+    = ValueAttendee
+    | ValueInvitation
+    | ValueFrame
 
 
 primaryColor : Color
@@ -148,8 +163,8 @@ menuAttrs =
     ]
 
 
-iconMenu : Model -> Int -> Attribute Msg
-iconMenu model id_ =
+iconMenuLeft : Model -> Int -> Attribute Msg
+iconMenuLeft model id_ =
     let
         id : Int
         id =
@@ -177,15 +192,63 @@ iconMenu model id_ =
                             , el [] <| html <| Material.Icons.settings 30 Material.Icons.Types.Inherit
                             ]
 
-                        ModalityFullscreen _ ->
-                            []
+                        ModalityEditing ->
+                            [ Input.button [] { label = el [] <| html <| Material.Icons.save 30 Material.Icons.Types.Inherit, onPress = Just <| MsgChangeModality <| ModalityNormal } ]
 
-                        _ ->
+                        ModalityFullscreen _ ->
+                            [ Input.button [] { label = el [] <| html <| Material.Icons.open_in_full 30 Material.Icons.Types.Inherit, onPress = Just <| MsgChangeModality <| ModalityFullscreen id }
+                            , Input.button [] { label = el [] <| html <| Material.Icons.edit 30 Material.Icons.Types.Inherit, onPress = Just <| MsgChangeModality <| ModalityEditing }
+                            , el [] <| html <| Material.Icons.open_in_new 30 Material.Icons.Types.Inherit
+                            , el [] <| html <| Material.Icons.settings 30 Material.Icons.Types.Inherit
+                            ]
+
+                        ModalitySettings ->
                             []
                    )
-                ++ [ el [ Font.size 28, moveDown 3 ] <| text <| Maybe.withDefault "" <| Dict.get (String.fromInt id) model.permanentState.attendees
+                ++ [ el [ Font.size 28, moveDown 3 ] <|
+                        text <|
+                            Maybe.withDefault "" <|
+                                Dict.get (String.fromInt id) model.permanentState.attendees
                    ]
             )
+
+
+viewFullscreen : Model -> Element Msg
+viewFullscreen model =
+    el
+        [ htmlAttribute <| Html.Attributes.style "width" "87vw"
+        , htmlAttribute <| Html.Attributes.style "height" "80vh"
+        , centerX
+        , centerY
+        , Background.color <| rgb 1 1 1
+        , Border.width spacingSize
+        , Border.color primaryColor
+        , iconMenuLeft model 0
+        , inFront <|
+            el (menuAttrs ++ [ alignRight, moveLeft 10 ]) <|
+                Input.button []
+                    { label = el [] <| html <| Material.Icons.close_fullscreen 30 Material.Icons.Types.Inherit
+                    , onPress = Just <| MsgChangeModality <| ModalityNormal
+                    }
+        ]
+    <|
+        html <|
+            Html.iframe
+                [ Html.Attributes.style "border" "0"
+                , Html.Attributes.style "height" "100%"
+                , Html.Attributes.style "background" "#4a0"
+
+                -- , Html.Attributes.src "https://07-elm-boot.lucamug.repl.co/"
+                , Html.Attributes.src "https://example.com/"
+
+                -- , Html.Attributes.src "https://repubblica.it/"
+                ]
+                []
+
+
+spacingSize : Int
+spacingSize =
+    10
 
 
 view : Model -> Html.Html Msg
@@ -197,50 +260,29 @@ view model =
 
         { x, y } =
             permanentStateToXY permanentState
-
-        spacingSize =
-            10
     in
     layout
         ([]
             ++ (case model.modality of
-                    ModalityFullscreen int ->
+                    ModalityFullscreen _ ->
                         [ inFront <|
-                            el
+                            Input.button
                                 [ width fill
                                 , height fill
                                 , padding 100
                                 , Background.color <| rgba 0 0 0 0.4
                                 ]
-                            <|
-                                el
-                                    [ width fill
-                                    , height fill
-                                    , Background.color <| rgb 1 1 1
-                                    , Border.width spacingSize
-                                    , Border.color primaryColor
-                                    , iconMenu model 0
-                                    , inFront <|
-                                        el (menuAttrs ++ [ alignRight, moveLeft 10 ]) <|
-                                            Input.button []
-                                                { label = el [] <| html <| Material.Icons.close_fullscreen 30 Material.Icons.Types.Inherit
-                                                , onPress = Just <| MsgChangeModality <| ModalityNormal
-                                                }
-                                    ]
-                                <|
-                                    html <|
-                                        Html.iframe
-                                            [ Html.Attributes.style "border" "0"
-                                            , Html.Attributes.style "height" "100%"
-                                            , Html.Attributes.style "background" "#4a0"
-
-                                            -- , Html.Attributes.src "https://07-elm-boot.lucamug.repl.co/"
-                                            , Html.Attributes.src "https://example.com/"
-
-                                            -- , Html.Attributes.src "https://repubblica.it/"
-                                            ]
-                                            []
+                                { label = text ""
+                                , onPress = Just <| MsgChangeModality ModalityNormal
+                                }
                         ]
+
+                    _ ->
+                        []
+               )
+            ++ (case model.modality of
+                    ModalityFullscreen int ->
+                        [ inFront <| viewFullscreen model ]
 
                     _ ->
                         []
@@ -255,58 +297,15 @@ view model =
             , padding spacingSize
             ]
             (List.indexedMap
-                (\indexX _ ->
+                (\indexY _ ->
                     row
                         [ width fill
                         , height fill
                         , spacing spacingSize
                         ]
                         (List.indexedMap
-                            (\indexY _ ->
-                                let
-                                    id =
-                                        (indexX + 1)
-                                            * (indexY + 1)
-                                in
-                                el
-                                    [ width <| fill
-                                    , height <| fill
-                                    , Background.color <| rgb 0 0.8 0
-                                    , inFront <|
-                                        row
-                                            [ spacing 5
-                                            , Font.color primaryColor
-                                            , Background.color <| rgba 1 1 1 0.5
-                                            , Border.rounded 10
-                                            , padding 5
-                                            , moveDown 10
-                                            , moveRight 10
-                                            ]
-                                            [ el
-                                                [ Font.size 36
-                                                , Font.bold
-                                                ]
-                                                (text <| String.fromInt id)
-                                            , Input.button [] { label = el [] <| html <| Material.Icons.open_in_full 30 Material.Icons.Types.Inherit, onPress = Just <| MsgChangeModality <| ModalityFullscreen id }
-                                            , el [] <| html <| Material.Icons.open_in_new 30 Material.Icons.Types.Inherit
-                                            , el [] <| html <| Material.Icons.edit 30 Material.Icons.Types.Inherit
-                                            , el [] <| html <| Material.Icons.settings 30 Material.Icons.Types.Inherit
-                                            , el [ Font.size 28 ] <| text <| Maybe.withDefault "" <| Dict.get (String.fromInt id) model.permanentState.attendees
-                                            ]
-                                    ]
-                                <|
-                                    html <|
-                                        Html.iframe
-                                            [ Html.Attributes.style "border" "0"
-                                            , Html.Attributes.style "height" "100%"
-                                            , Html.Attributes.style "background" "#4a0"
-
-                                            -- , Html.Attributes.src "https://07-elm-boot.lucamug.repl.co/"
-                                            , Html.Attributes.src "https://example.com/"
-
-                                            -- , Html.Attributes.src "https://repubblica.it/"
-                                            ]
-                                            []
+                            (\indexX _ ->
+                                viewFrame model ((indexX + 1) + (indexY * x))
                             )
                             (List.repeat x 0)
                         )
@@ -315,33 +314,73 @@ view model =
             )
 
 
+viewFrame : Model -> Int -> Element Msg
+viewFrame model id =
+    el
+        ([ width <| fill
+         , height <| fill
+         , Background.color <| rgb 0 0.8 0
+         ]
+            ++ (case model.modality of
+                    ModalityEditing ->
+                        [ inFront <|
+                            column
+                                [ width fill
+                                , height fill
+                                , Background.color <| rgba 0 0 0 0.4
+                                , paddingEach { top = 80, right = 10, bottom = 0, left = 10 }
+                                , spacing 10
+                                ]
+                            <|
+                                [ inputField { existingData = model.permanentState.attendees, id = id, label = "Attendee", valueType = ValueAttendee }
+                                , inputField { existingData = model.permanentState.attendees, id = id, label = "Invitation", valueType = ValueInvitation }
+                                , inputField { existingData = model.permanentState.attendees, id = id, label = "Frame", valueType = ValueFrame }
+                                ]
+                        ]
 
--- column []
---     ([ text "Hi"
---      , Input.text []
---         { label = Input.labelHidden ""
---         , onChange = MsgChangeX
---         , placeholder = Nothing
---         , text = permanentState.x
---         }
---      , Input.text []
---         { label = Input.labelHidden ""
---         , onChange = MsgChangeY
---         , placeholder = Nothing
---         , text = permanentState.y
---         }
---      ]
---         ++ List.indexedMap
---             (\index _ ->
---                 Input.text []
---                     { label = Input.labelHidden ""
---                     , onChange = MsgChangeAttendee index
---                     , placeholder = Nothing
---                     , text = Maybe.withDefault "" <| List.Extra.getAt index permanentState.attendees
---                     }
---             )
---             (List.repeat (xy.x * xy.y) ())
---     )
+                    _ ->
+                        []
+               )
+            ++ [ iconMenuLeft model id ]
+        )
+    <|
+        html <|
+            Html.iframe
+                [ Html.Attributes.style "border" "0"
+                , Html.Attributes.style "height" "100%"
+                , Html.Attributes.style "background" "#4a0"
+
+                -- , Html.Attributes.src "https://07-elm-boot.lucamug.repl.co/"
+                , Html.Attributes.src "https://example.com/"
+
+                -- , Html.Attributes.src "https://repubblica.it/"
+                ]
+                []
+
+
+inputField :
+    { existingData : Dict.Dict String String
+    , id : Int
+    , label : String
+    , valueType : Value
+    }
+    -> Element Msg
+inputField { label, valueType, existingData, id } =
+    row
+        [ Background.color <| rgba 1 1 1 0.8
+        , padding 5
+        , spacing 10
+        , Border.rounded 10
+        , width fill
+        ]
+        [ el [ width <| px 90, Font.alignRight ] <| text label
+        , Input.text [ Border.rounded 6 ]
+            { onChange = MsgChangeValue valueType (String.fromInt id)
+            , text = Maybe.withDefault "" <| Dict.get (String.fromInt id) existingData
+            , placeholder = Nothing
+            , label = Input.labelHidden label
+            }
+        ]
 
 
 permanentStateToXY : { a | x : String, y : String } -> { x : Int, y : Int }
